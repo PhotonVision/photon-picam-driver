8b8b37b92d4ae7dcfd596aa3611382f9de5c2bf3
= <matthew.morley.ca@gmail.com>
2020-07-20 23:01:27 +0100

    copoy stuff

--- PicamJNI.c
+++ PicamJNI.c
@@ -1,4 +1,9 @@
 #include <jni.h>        // JNI header provided by JDK
 #include <stdio.h>      // C Standard IO Header
-#include "org_photonvision_raspi_PicamJNI"   // Generated
+#include "PicamJNI.h"   // Generated
 
+// Implementation of the native method sayHello()
+JNIEXPORT void JNICALL Java_org_photonvision_raspi_PicamJNI_createCamera(JNIEnv *env, jobject thisObj) {
+   printf("Hello World!\n");
+   return;
+}
