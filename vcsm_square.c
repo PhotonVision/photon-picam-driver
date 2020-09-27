1c9133334de613c749212112b956422dc05b611d
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2020-09-27 13:53:10 -0700

    Make licensing notices consistient

--- vcsm_square.c
+++ vcsm_square.c
@@ -1,55 +1,19 @@
 /*
-Copyright (c) 2013, Broadcom Europe Ltd
-Copyright (c) 2016, Tim Gover
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the copyright holder nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
-/* Make the render output CPU accessible by defining a framebuffer texture
- * stored in a VCSM (VideoCore shared memory) EGL image.
+ * Copyright (C) 2020 Photon Vision.
  *
- * This example just demonstrates how to use use the APIs by using the CPU.
- * to blit an animated rectangle into frame-buffer texture in shared memory.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * A more realistic example would be to do a blur, edge-detect in GLSL then pass
- * the buffer to OpenCV. There may be some benefit in using multiple GL contexts
- * to reduce the impact of serializing operations with a glFlush.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * N.B VCSM textures are raster scan order textures. This makes it very
- * convenient to read and modify VCSM frame-buffer textures from the CPU.
- * However, if the output of the CPU stage is drawn again as a texture that
- * is rotated or scaled then it can sometimes be better to use glTexImage2D
- * to allow the driver to convert this back into the native texture format.
- *
- * Example usage
- * raspistill -p 0,0,1024,1024 -gw 0,0,1024,1024 -t 10000 --gl -gs vcsm_square
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-/* Uncomment the next line to compare with the glReadPixels implementation. VCSM
- * should run at about 40fps with a 1024x1024 texture compared to about 20fps
- * using glReadPixels.
- */
-//#define USE_READPIXELS
 
 #include "vcsm_square.h"
 #include "RaspiTex.h"
